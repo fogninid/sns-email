d9304a03350a690ae8880b8b3698b76c5aa78df1
Daniele Fognini <dfogni@gmail.com>
2021-04-23 00:27:04 +0200

    release 0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sns_email",
-    version="0.3.2",
+    version="0.3.3",
     author="https://github.com/fogninid",
     url="https://github.com/fogninid/sns-email",
     python_requires=">=3.8",
